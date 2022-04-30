871d6142b1837eef106231190687f7a495719642
Shantanu Gupta <shantanu.gupta@singtel.com>
2022-04-30 16:49:16 +0800

    Created the timezone bean

--- src/main/java/per/shantanu/poc/sailpoint/Application.java
+++ src/main/java/per/shantanu/poc/sailpoint/Application.java
@@ -1,9 +1,13 @@
 package per.shantanu.poc.sailpoint;
 
+import lombok.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import per.shantanu.poc.sailpoint.config.ApplicationProperties;
+
+import java.time.ZoneId;
 
 @SpringBootApplication(scanBasePackages = "per.shantanu")
 @EnableConfigurationProperties(ApplicationProperties.class)
@@ -11,5 +15,10 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ZoneId timezone(@NonNull ApplicationProperties properties) {
+        return properties.timezone();
     }
 }
