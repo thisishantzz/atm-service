d2857ed35b59e776f159b58d223f58e3656b43b1
Shantanu Gupta <shantanu.gupta@singtel.com>
2022-04-30 16:07:22 +0800

    Created the application runner

--- src/main/java/per/shantanu/poc/sailpoint/Application.java
+++ src/main/java/per/shantanu/poc/sailpoint/Application.java
@@ -1,8 +1,12 @@
 package per.shantanu.poc.sailpoint;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "per.shantanu")
 public class Application {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(Application.class, args);
     }
 }
