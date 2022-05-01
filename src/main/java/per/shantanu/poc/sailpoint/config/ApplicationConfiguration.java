2bc4d153681f7517126a2fdad6f79b7ea26ee27a
Shantanu Gupta <shantanu.gupta@singtel.com>
2022-05-01 12:26:17 +0800

    Added a configuration class that configures webflux

---
+++ src/main/java/per/shantanu/poc/sailpoint/config/ApplicationConfiguration.java
@@ -0,0 +1,31 @@
+package per.shantanu.poc.sailpoint.config;
+
+import java.time.ZoneId;
+import lombok.NonNull;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.codec.CodecCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration(proxyBeanMethods = false)
+public class ApplicationConfiguration {
+
+  @Bean
+  public WebFluxConfigurer webFluxConfigurer(ObjectProvider<CodecCustomizer> codecCustomizers) {
+    return new WebFluxConfigurer() {
+      @Override
+      public void configureHttpMessageCodecs(@NonNull ServerCodecConfigurer configurer) {
+        codecCustomizers.orderedStream().forEach(c -> c.customize(configurer));
+      }
+    };
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public ZoneId applicationTimezone(ApplicationProperties properties) {
+    return properties.timezone();
+  }
+}
