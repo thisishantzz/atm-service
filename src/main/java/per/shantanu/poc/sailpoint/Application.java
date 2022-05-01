48c9de40a1f6be086d511bb69e0f83403f9a1fa4
Shantanu Gupta <shantanu.gupta@singtel.com>
2022-05-01 11:19:53 +0800

    Ran spotlessApply

--- src/main/java/per/shantanu/poc/sailpoint/Application.java
+++ src/main/java/per/shantanu/poc/sailpoint/Application.java
@@ -1,5 +1,6 @@
 package per.shantanu.poc.sailpoint;
 
+import java.time.ZoneId;
 import lombok.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,18 +8,16 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import per.shantanu.poc.sailpoint.config.ApplicationProperties;
 
-import java.time.ZoneId;
-
 @SpringBootApplication(scanBasePackages = "per.shantanu")
 @EnableConfigurationProperties(ApplicationProperties.class)
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
-    @Bean
-    public ZoneId timezone(@NonNull ApplicationProperties properties) {
-        return properties.timezone();
-    }
+  @Bean
+  public ZoneId timezone(@NonNull ApplicationProperties properties) {
+    return properties.timezone();
+  }
 }
