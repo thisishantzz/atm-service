8c9e45f0293ceb7403dbb7818b394755780aabc5
Shantanu Gupta <shantanu.gupta@singtel.com>
2022-05-01 15:10:08 +0800

    Created a bean that enhances the jackson object mapper with custom serializers and deserializers

--- src/main/java/per/shantanu/poc/sailpoint/config/ApplicationConfiguration.java
+++ src/main/java/per/shantanu/poc/sailpoint/config/ApplicationConfiguration.java
@@ -4,12 +4,19 @@ import java.time.ZoneId;
 import lombok.NonNull;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.codec.CodecCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import per.shantanu.poc.sailpoint.entities.Account;
+import per.shantanu.poc.sailpoint.entities.Credentials;
+import per.shantanu.poc.sailpoint.handlers.ATMServiceController;
+import per.shantanu.poc.sailpoint.internal.jackson.AccountSerializer;
+import per.shantanu.poc.sailpoint.internal.jackson.AmountDeserializer;
+import per.shantanu.poc.sailpoint.internal.jackson.CredentialDeserializer;
 
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties(ApplicationProperties.class)
@@ -29,5 +36,14 @@ public class ApplicationConfiguration {
   @ConditionalOnMissingBean
   public ZoneId applicationTimezone(ApplicationProperties properties) {
     return properties.timezone();
+  }
+
+  @Bean
+  public Jackson2ObjectMapperBuilderCustomizer enhanceObjectMapper() {
+    return builder -> {
+      builder.deserializerByType(Credentials.class, new CredentialDeserializer());
+      builder.deserializerByType(ATMServiceController.Amount.class, new AmountDeserializer());
+      builder.serializerByType(Account.class, new AccountSerializer());
+    };
   }
 }
